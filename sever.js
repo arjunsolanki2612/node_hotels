b3489626fb3a6762df626e9ed5ca8707dee89e22
Arjun Solanki <arjunsolanki2148@gmail.com>
2024-01-21 16:25:12 +0530

    add env file and db shifted to MongoDB online server

--- sever.js
+++ sever.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
-
+require("dotenv").config();
 const bodyParser = require("body-parser");
 app.use(bodyParser.json()); // stores in req.body
 
@@ -57,6 +57,7 @@ const menuItemRoutes = require("./routes/menuItemRoutes");
 app.use("/person", personRoutes);
 app.use("/menu", menuItemRoutes);
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
   console.log("server is started");
 });
